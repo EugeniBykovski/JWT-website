3aa759cc2697ca068979c4fa1ca01919b8db259a
EugeniBykovski <bykovskieugeni@mail.ru>
2023-06-08 01:19:58 +0200

    added all functionality

--- src/js/scroll_up.js
+++ src/js/scroll_up.js
@@ -1,17 +1,17 @@
 const btnUp = {
-  el: document.querySelector(".btns__up"),
+  el: document.querySelector(".btn__up"),
   show() {
-    this.el.classList.remove("btns__up_hide");
+    this.el.classList.remove("btn__up_hide");
   },
   hide() {
-    this.el.classList.add("btns__up_hide");
+    this.el.classList.add("btn__up_hide");
   },
   addEventListener() {
     window.addEventListener("scroll", () => {
       const scrollY = window.scrollY || document.documentElement.scrollTop;
       scrollY > 80 ? this.show() : this.hide();
     });
-    document.querySelector(".btns__up").onclick = () => {
+    document.querySelector(".btn__up").onclick = () => {
       window.scrollTo({
         top: 0,
         left: 0,
