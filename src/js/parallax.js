34f80dec4a6e9b118a9b610af0cbf22839ba92bd
EugeniBykovski <bykovskieugeni@mail.ru>
2023-06-14 11:01:37 +0200

    added parallax effect

---
+++ src/js/parallax.js
@@ -0,0 +1,18 @@
+const scene = document.getElementById("scene");
+const scene2 = document.getElementById("scene2");
+
+const parallaxInstance = new Parallax(scene, {
+  relativeInput: true,
+  invertX: false,
+  invertY: false,
+  limitX: false,
+  limitY: false,
+  frictionX: 0.07,
+  frictionY: 0.07,
+});
+
+const parallaxInstance2 = new Parallax(scene2, {
+  relativeInput: true,
+  frictionX: 0.09,
+  frictionY: 0.09,
+});
